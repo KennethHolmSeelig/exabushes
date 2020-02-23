6db76d7d49918303d8be65f5d4940b58e688f732
Kenneth <kenneth@reflective.dk>
2020-02-23 12:20:57 +0000

    trigger

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 in js client add alias
 ´´´js
+window.gwc = gwc;
 function reqListener () {
   eval(this.responseText);
 }
@@ -11,3 +12,5 @@ oReq.addEventListener("load", reqListener);
 oReq.open("GET", "https://cdn.jsdelivr.net/gh/KennethHolmSeelig/exabushes/compiled.js");
 oReq.send();
 ´´´
+
+execute the alias in the tower of realms, in console you will see quests to finish.
--- compiled.js
+++ compiled.js
@@ -1,1 +1,35 @@
-console.log('loaded');
+if (!gwc) {
+  var gwc = window.gwc;
+}
+gwc.connection.send('quests');
+function quests() {
+    var questMap = {
+	  'Orc Figurine': false,
+	  'Enchant thing': false
+	};
+	var lines = document.getElementsByClassName('line');
+
+	let questLineFound = false;
+	for(var i = 0; i < lines.length;i++) {
+	  var line = lines[i];
+		if (questLineFound) {
+		  let questKeys = Object.keys(questMap);
+		  for (var k = 0; k < questKeys.length; k++) {
+		    if (line.textContent.indexOf(questKeys[k]) > -1){
+			   questMap[questKeys[k]] = true;
+			}
+		  }
+		} else {
+			if (line.textContent.indexOf('quests in Sparkle') -1) {
+				questLineFound = true;
+			}
+		}
+	}
+    let questKeys = Object.keys(questMap);
+	console.log('You need to finish:');
+	for (var k = 0; k < questKeys.length; k++) {
+	  if (questMap[questKeys[k]] === false) {
+		console.log(questKeys[k]);
+	  }
+	}
+}
