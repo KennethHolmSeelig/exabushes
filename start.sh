e20174e3bc5237fa7875deb81f7855c9018e58cb
Kenneth <kenneth@reflective.dk>
2020-03-11 07:26:17 +0000

    the server thing

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 git pull
-if [ -z "$1" ]
-  then
-    echo "Supply discord bot token, maybe $DISCORD_TOKEN ?"
-else
-  sudo node src/index.js $1
-fi
+sudo systemctl stop exabushes
+sudo systemctl start exabushes
